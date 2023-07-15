c72ab24fb313d72efb1780f9f978f11ef1aa8c5f
Jonathan Araujo <jonathan@wicadu.com>
2023-07-15 11:07:45 -0400

    Add ProtectedRoute hoc

---
+++ src/components/HOCs/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import React, { useMemo } from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+function ProtectedRoute({ children }: any) {
+  const thereIsAToken = useMemo(() => localStorage.getItem('token'), [])
+
+  if (!thereIsAToken) return <Navigate to='/' replace />
+
+  return children || <Outlet />
+
+}
+
+export default ProtectedRoute
