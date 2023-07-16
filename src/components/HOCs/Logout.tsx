be29e260479b631bf6b06c2822b1336c6f7f81db
Jonathan Araujo <jonathan@wicadu.com>
2023-07-16 13:59:51 -0400

    Update useFetch [Hook] and Logunt [Hoc]

--- src/components/HOCs/Logout.tsx
+++ src/components/HOCs/Logout.tsx
@@ -1,10 +1,12 @@
-import React, { useCallback } from 'react'
+import { useCallback } from 'react'
 
 import { useNavigate } from 'react-router-dom'
 import Button from '../atoms/Button'
 
 function Logout() {
   const navigate = useNavigate()
+
+  // If there's an api call to trigger when logout, it shold be handle in this file.
 
   const handleLogout = useCallback(() => {
     localStorage.removeItem('token')
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,27 +1,27 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 type Response = {
   data: null | any;
   error: null | Error | any;
-};
+}
 
 export default  function (url: string, options?: RequestInit): Response {
   const [data, setData] = useState<Response['data']>(null)
   const [error, setError] = useState<Response['error']>(null)
 
   useEffect(() => {
-      fetch(url, options)
-        .then(async res => {
-          const data = await res.json()
+    fetch(url, options)
+      .then(async res => {
+        const data = await res.json()
 
-          if (res.status < 200 || res.status > 299) {
-            throw new Error(data?.message)
-          }
+        if (res.status < 200 || res.status > 299) {
+          throw new Error(data?.message)
+        }
 
-          return data
-        })
-        .then((data) => setData(data))
-        .catch((error) => setError(error))
+        return data
+      })
+      .then((data) => setData(data))
+      .catch((error) => setError(error))
   }, [])
 
   return { data, error }
