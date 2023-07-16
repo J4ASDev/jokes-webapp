d1441e83f2e8dd259fbf7051bd3283be489448b1
Jonathan Araujo <jonathan@wicadu.com>
2023-07-16 16:53:50 -0400

    Update useFetch hook to handle trigger requests

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 
 type Response = {
   data: null | any;
   error: null | Error | any;
 }
 
-export default  function (url: string, options?: RequestInit): Response {
+export type HookResponse = [Response, (url: string, options?: RequestInit) => Promise<any>]
+
+export default  function (): HookResponse {
   const [data, setData] = useState<Response['data']>(null)
   const [error, setError] = useState<Response['error']>(null)
 
-  useEffect(() => {
-    fetch(url, options)
+  const getData = useCallback(async (url: string, options?: RequestInit): Promise<any> => {    
+    return await fetch(url, options)
       .then(async res => {
         const data = await res.json()
 
@@ -20,9 +22,19 @@ export default  function (url: string, options?: RequestInit): Response {
 
         return data
       })
-      .then((data) => setData(data))
-      .catch((error) => setError(error))
+      .then((data) => {
+        setData(data)
+        setError(null)
+
+        return { data, error: null }
+      })
+      .catch((error) => {
+        setData([])
+        setError(error)
+
+        return { data: [], error }
+      })
   }, [])
 
-  return { data, error }
+  return [{ data, error }, getData]
 }
