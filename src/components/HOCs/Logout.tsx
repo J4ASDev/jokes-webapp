9a632c84812775f98f5982d4e1c586c692ab83a7
Jonathan Araujo <jonathan@wicadu.com>
2023-07-17 16:05:23 -0400

    Add cookies-js and check if token is saved in cookies

--- src/components/HOCs/Logout.tsx
+++ src/components/HOCs/Logout.tsx
@@ -1,17 +1,18 @@
 import { useCallback } from 'react'
-
 import { useNavigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
+
 import Button from '../atoms/Button'
 
 function Logout() {
   const navigate = useNavigate()
 
   // If there's an api call to trigger when logout, it shold be handle in this file.
-
   const handleLogout = useCallback(() => {
     localStorage.removeItem('token')
+    Cookies.remove('token')
     navigate('/')
-  }, [])
+  }, [navigate])
 
   return (
     <Button
--- src/components/HOCs/ProtectedRoute.tsx
+++ src/components/HOCs/ProtectedRoute.tsx
@@ -1,8 +1,9 @@
 import { useMemo } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 function ProtectedRoute({ children }: any) {
-  const thereIsAToken = useMemo(() => localStorage.getItem('token'), [])
+  const thereIsAToken = useMemo(() => localStorage.getItem('token') || Cookies.get('token'), [])
 
   if (!thereIsAToken) return <Navigate to='/' replace />
 
